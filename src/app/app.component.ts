e82e7c35a39368c122e6359e8ab8bd4d76a5c74e
Joan Sweeney <joanms@eircom.net>
2019-09-17 16:12:09 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  displayText = false;
+  log = [];
+
+  onToggleText() {
+    this.displayText = !this.displayText;
+    this.log.push(this.log.length + 1);
+  }
+}
